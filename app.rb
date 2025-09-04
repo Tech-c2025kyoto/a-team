6871f8662972cd26e3d51eba7b336b451e099822
ktc <ktc2243050001@edu.kyoto-tech.ac.jp>
2025-09-04 19:26:57 +0900

    リトライ機能の修正

--- app.rb
+++ app.rb
@@ -6,9 +6,16 @@ class App
       game = Game.new
       game.start
 
-      print 'もう一度プレイしますか？ (y/n): '
-      answer = gets.chomp.downcase
-      break unless answer == 'y'
+      # ▼ y/n 以外は再入力させるループ
+      retry_input = nil
+      loop do
+        print 'もう一度プレイしますか？ (y/n): '
+        retry_input = gets.chomp.downcase
+        break if %w[y n].include?(retry_input)
+        puts '無効な入力です。y または n を入力してください。'
+      end
+
+      break if retry_input == 'n'
     end
 
     puts 'ゲームを終了します。ありがとうございました！'
